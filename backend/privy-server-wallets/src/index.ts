97b5e5f7a253b3b40106ee41673079033d465f7f
“Karthik144” <=>
2025-03-04 16:30:26 -0500

    Added logic to post newly created privy server wallet data to db in users table

--- backend/privy-server-wallets/src/index.ts
+++ backend/privy-server-wallets/src/index.ts
@@ -1,5 +1,7 @@
 import { PrivyClient } from "@privy-io/server-auth";
 import { PrivyServerWallet } from "./types";
+import { supabase } from "./supabaseClient";
+
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -22,9 +24,40 @@ async function createPrivyServerWallet(): Promise<PrivyServerWallet> {
   }
 }
 
+export async function createUserWithWallet(
+  walletInfo: PrivyServerWallet
+): Promise<boolean> {
+  console.log("Currently creating a new user with privy server wallet");
+
+  const { data, error } = await supabase
+    .from("users")
+    .insert({
+      privy_server_wallet_info: {
+        id: walletInfo.id,
+        address: walletInfo.address,
+        chain_type: walletInfo.chainType,
+      },
+    })
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error(`User creation failed: ${error.message}`);
+  }
+
+  return true;
+}
+
 async function main() {
   const newlyCreatedPrivyServerWallet = await createPrivyServerWallet();
   console.log("Newly Created Wallet:", newlyCreatedPrivyServerWallet);
+  const hasCreatedNewUser = await createUserWithWallet(
+    newlyCreatedPrivyServerWallet
+  );
+
+  if (hasCreatedNewUser) {
+    console.log("Successfully created new user in db");
+  }
 }
 
 main();
